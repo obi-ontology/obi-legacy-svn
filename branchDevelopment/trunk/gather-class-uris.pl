f3f571df3216a3429f8c1eff4eea0e1b0e796e84
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2008-03-02 08:32:50 +0000

    normalize the namespace declarations across all the files, arranged so that protege doesn't make up any of it's own namespace prefixes
    git-svn-id: http://svn.code.sf.net/p/obi/code@359 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- branchDevelopment/trunk/gather-class-uris.pl
+++ branchDevelopment/trunk/gather-class-uris.pl
@@ -1,9 +1,11 @@
 use strict;
 
-
 open TOP, "<obi.owl" or die("can't find obi.owl");
 my @lines = <TOP>;
 close TOP;
+my $include_existing =1;
+my $copydir = "newids/";
+my $newNamespace = "http://purl.org/obo/";
 
 # extract the names of the branches
 my @obiParts = grep {/<owl:imports rdf:resource="http:\/\/obi.sourceforge.net\/ontology\/OBI\/(.*)\.owl"/;$1} @lines;
@@ -12,12 +14,13 @@ my @obiParts = map { s/.*\/([A-Za-z.-]*)\.owl.*/$1/;chomp $_ ; $_} @obiParts;
 # extract the other import statementsn
 my @otherImports = grep {/<owl:imports rdf:resource="(.*?)"/;my $it=$1; if ($_=~/obi\.sourceforge\.net/) {undef} else {$it}} @lines;
 
-my @all;
+my @replacements;
+my %replacements;
 
 sub findnames 
   {
     foreach my $part ( @obiParts) {
-      next if ($part=~/Relation/); 
+      next if ($part=~/Relation|AnnotationProperty|owl-full/); 
       my $path = "$part".".owl";
       open PART, "<$path" or die("Trouble loading $path");
       my $copy;
@@ -26,9 +29,10 @@ sub findnames
 	if (/<\/rdf:RDF>/) { $copy = 0 } # but don't copy the closing tag
 	if ($copy) 
 	  { 
-	    my @names = $_ =~ /rdf:(about|id|resource)=(".*?")/g;
-	    my %them  = @names;
-	    @all = (grep {/obi\.sourceforge/ && !/OBI_/} (values %them),@all);
+	    my @names = $_ =~ /rdf:(about|id|resource)="(.*?)"/g;
+#	    my %them  = @names;
+	    @replacements = (grep {/obi\.sourceforge/ && ($include_existing || !/OBI_/)} @names,@replacements);
+#	    @replacements = (@names,@replacements);
 	    print OUT $_ 
 	  }
       }
@@ -36,9 +40,39 @@ sub findnames
     }
   }
 
-  findnames();
+sub replacenames
+  {
+    my $count=1;
 
-my %all;
-foreach my $name(@all) { $all{$name}=1}
+    foreach my $name(@replacements)
+      { if (!$replacements{$name})
+	  { $replacements{$name}= sprintf("$newNamespace\OBI_%06d",$count++); }
+      };
 
-print join("\n",keys %all);
+    foreach my $part ( @obiParts) {
+      my $path = "$part".".owl";
+      open PART, "<$path" or die("Trouble loading $path");
+      my $copypath = $copydir.$part.".owl";
+      open PARTCOPY, ">$copypath" or die ("Trouble writing $path");
+      my $copy;
+      while (<PART>) {
+	if (/<\/owl:Ontology>/) { print PARTCOPY $_; $copy = 1; next } # don't start copying until after the <ontology> section
+	if (/<\/rdf:RDF>/) { $copy = 0 } # but don't copy the closing tag
+	if ($copy) 
+	  { 
+	    s/rdf:(about|id|resource)="(.*?)"/"rdf:".$1."=\"".($replacements{$2}||$2)."\""/ge;
+	    s/http:\/\/obi.sourceforge.net\/ontology\/OBI.owl#/$newNamespace/;
+	  }
+	print PARTCOPY $_; 
+      }
+      close PART;
+      close PARTCOPY;
+    }
+  }
+
+findnames();
+replacenames();
+
+$DB::single=1;1;
+
+print join("\n",keys %replacements);
