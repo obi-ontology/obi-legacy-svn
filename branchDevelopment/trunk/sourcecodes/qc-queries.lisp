9eeeb9878e2443feaf4863ab21b413c897744793
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2008-04-01 18:15:10 +0000

    don't show sparql for qc queries
    git-svn-id: http://svn.code.sf.net/p/obi/code@474 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- branchDevelopment/trunk/sourcecodes/qc-queries.lisp
+++ branchDevelopment/trunk/sourcecodes/qc-queries.lisp
@@ -12,7 +12,7 @@
       (?si !rdfs:label ?s)
       )
      )
-   :kb kb :use-reasoner :jena :trace t :values nil))
+   :kb kb :use-reasoner :jena :trace "Curation status" :values nil :trace-show-query nil))
 
 (defun missing-curation (kb)
   (sparql
@@ -34,7 +34,30 @@
 	       (not (equal ?type !rdfs:Class))
 	       (not (bound ?status))))
      )
-   :kb kb :use-reasoner :jena :trace t :values nil))
+   :kb kb :use-reasoner :jena :trace "Terms missing curation status" :values nil :trace-show-query nil))
+
+;; (defun string-curation-status (kb)
+;;   (sparql
+;;    '(:select (?si ?s ?type) (:distinct t)
+;;      (?p !rdfs:label |\"curation status\"@en|)
+;;      (?p :a !owl:AnnotationProperty)
+;;      (:union
+;;       ((?si !rdf:type !owl:AnnotationProperty))
+;;       ((?si !rdf:type !owl:ObjectProperty))
+;;       ((?si !rdf:type !owl:DatatypeProperty))
+;;       ((?si !rdf:type !owl:Class))
+;;      )
+;;      (?si !rdfs:label ?s)
+;;      (?si :a ?type)
+;;      (:optional (?si ?p ?status))
+;;      (:filter (and (regex (str ?si) "obi|OBI")
+;; 	       (not (equal ?type !rdf:Property))
+;; 	       (not (equal ?type !owl:Thing))
+;; 	       (not (equal ?type !rdfs:Class))
+;; 	       (not (bound ?status))
+;; 	       (not (isuri ?status))))
+;;      )
+;;    :kb kb :use-reasoner :none :trace "Terms with strings as curation status" :values nil :trace-show-query nil))
 
 (defun untranslated-uris (kb)
   (sparql
@@ -48,7 +71,7 @@
      )
      (:filter (and (regex (str ?si) "obi|OBI") (not (regex (str ?si) "http://purl.obofoundry.org") ))
      ))
-   :kb kb :use-reasoner :jena :trace "old obi ids" :values nil)
+   :kb kb :use-reasoner :jena :trace "old obi ids" :values nil :trace-show-query nil)
   (sparql
    '(:select (?si ?s) (:distinct t)
      (?si !rdfs:label ?s)
@@ -59,7 +82,7 @@
       ((?si !rdf:type !owl:Class))
      )
      (:filter (regex (str ?si) "http://www.geneontology.org/formats/oboInOwl#")))
-   :kb kb :use-reasoner :jena :trace "geneontology ids" :values nil)
+   :kb kb :use-reasoner :jena :trace "geneontology ids" :values nil :trace-show-query nil)
   (sparql
    '(:select (?si ?s) (:distinct t)
      (?si !rdfs:label ?s)
@@ -70,7 +93,20 @@
       ((?si !rdf:type !owl:Class))
      )
      (:filter (regex (str ?si) "Class_\\d+")))
-   :kb kb :use-reasoner :jena :trace "ids that look like Class_12" :values nil)
+   :kb kb :use-reasoner :jena :trace "ids that look like Class_12" :values nil :trace-show-query nil)
+  (sparql
+   '(:select (?si ?s) (:distinct t)
+     (?si !rdfs:label ?s)
+     (:union
+      ((?si !rdf:type !owl:AnnotationProperty))
+      ((?si !rdf:type !owl:ObjectProperty))
+      ((?si !rdf:type !owl:DatatypeProperty))
+      ((?si !rdf:type !owl:Class))
+      )
+     (:filter (and (regex (str ?si) "http://purl.obofoundry.org")
+	       (not (regex (str ?si) "http://purl.obofoundry.org/obo/OBI_\\d{7}") )))
+      )
+   :kb kb :use-reasoner :jena :trace "uris without ids assigned yet" :values nil :trace-show-query nil)
   )
 
 (defun next-unused-id (kb &optional (howmany 1))
@@ -109,4 +145,4 @@
 	       (not (equal ?type !rdfs:Class))
 	       (not (bound ?label))))
      )
-   :kb kb :use-reasoner :jena :trace t :values nil))
+   :kb kb :use-reasoner :jena :trace "Missing labels" :values nil :trace-show-query nil))
