e82c31a2fd646853b22ed9b58dc879497973b9a2
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2008-04-07 04:28:50 +0000

    starting to rearrange to conform with new svn structure
    git-svn-id: http://svn.code.sf.net/p/obi/code@502 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- branchDevelopment/trunk/sourcecodes/write-purls.lisp
+++ branchDevelopment/trunk/sourcecodes/write-purls.lisp
@@ -1,37 +1,40 @@
 (defun write-purls (kb kbprev &optional (dest "obi:list-purls.xml"))
   (let((kb-purls
-	 (sparql
-	  '(:select (?thing) (:distinct t)
-	    (:union
-	     ((?thing ?p ?o))
-	     ((?s ?thing ?o))
-	     ((?s ?p ?thing)))
-	    (:filter (regex (str ?thing) "OBI_\\d+"))
-	    )
-	  :kb kb :use-reasoner :none :flatten t))
-	(kbprev-purls
-	 (and kbprev
-	      (sparql
-	       '(:select (?thing) (:distinct t)
-		 (:union
-		  ((?thing ?p ?o))
-		  ((?s ?thing ?o))
-		  ((?s ?p ?thing)))
-		 (:filter (regex (str ?thing) "OBI_\\d+"))
-		 )
-	       :kb kbprev :use-reasoner :none :flatten t))))
+	(sparql
+	 '(:select (?thing) (:distinct t)
+	   (:union
+	    ((?thing ?p ?o))
+	    ((?s ?thing ?o))
+	    ((?s ?p ?thing)))
+	   (:filter (regex (str ?thing) "OBI_\\d+"))
+	   )
+	 :kb kb :use-reasoner :none :flatten t))
+       (kbprev-purls
+	(and kbprev
+	     (sparql
+	      '(:select (?thing) (:distinct t)
+		(:union
+		 ((?thing ?p ?o))
+		 ((?s ?thing ?o))
+		 ((?s ?p ?thing)))
+		(:filter (regex (str ?thing) "OBI_\\d+"))
+		)
+	      :kb kbprev :use-reasoner :none :flatten t))))
     
-  (with-open-file (out dest
-		       :direction :output
-		       :if-exists :supersede)
-    (format out "<recs>~%")
-    (loop for new in (set-difference kb-purls kbprev-purls)
-       for purl = (#"replaceFirst" (uri-full new) ".*/obo/" "/obo/")
-       for id = (#"replaceFirst" purl ".*/" "")
-       do (format out "<rec><purl>~a</purl><url>http://sw.neurocommons.org/obiterm/~a</url><id>CMUNGALL</id><id>MELANIE</id><type>User_Batch_Add</type></rec>~%" purl id ))
-    (format out "</recs>~%")
-)
-))
+    (with-open-file (out dest
+			 :direction :output
+			 :if-exists :supersede)
+      (format out "<recs>~%")
+      (loop for new in (set-difference kb-purls kbprev-purls)
+	 for purl = (#"replaceFirst" (uri-full new) ".*/obo/" "/obo/")
+	 for id = (#"replaceFirst" purl ".*/" "")
+	 do (format out "<rec><purl>~a</purl><url>http://sw.neurocommons.org/obiterm/~a</url><id>CMUNGALL</id><id>MELANIE</id><type>User_Batch_Add</type></rec>~%" purl id ))
+      (format out "</recs>~%")
+      )
+    (when (set-difference kbprev-purls kb-purls)
+      (format t "Hmm, we seem to have lost some ids (deprecation lossage?): ~%~{~a~%~}"
+	  (set-difference kbprev-purls kb-purls)))    
+    ))
 
 
 
