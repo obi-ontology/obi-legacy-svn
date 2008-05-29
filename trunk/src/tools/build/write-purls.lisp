73c6c1c7b22200c3f0656381fd7f0bd37dfd390e
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2008-05-29 22:36:40 +0000

    bugfix
    git-svn-id: http://svn.code.sf.net/p/obi/code@671 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- trunk/src/tools/build/write-purls.lisp
+++ trunk/src/tools/build/write-purls.lisp
@@ -6,7 +6,7 @@
 	    ((?thing ?p ?o))
 	    ((?s ?thing ?o))
 	    ((?s ?p ?thing)))
-	   (:filter (regex (str ?thing) "OBI_\\d+"))
+	   (:filter (and (isuri ?thing) (regex (str ?thing) "OBI_\\d+")))
 	   )
 	 :kb kb :use-reasoner :none :flatten t))
        (kbprev-purls
@@ -17,24 +17,29 @@
 		 ((?thing ?p ?o))
 		 ((?s ?thing ?o))
 		 ((?s ?p ?thing)))
-		(:filter (regex (str ?thing) "OBI_\\d+"))
+		(:filter (and (isuri ?thing) (regex (str ?thing) "OBI_\\d+")))
 		)
 	      :kb kbprev :use-reasoner :none :flatten t))))
-    
-    (with-open-file (out dest
-			 :direction :output
-			 :if-exists :supersede)
-      (format out "<recs>~%")
-      (loop for new in (set-difference kb-purls kbprev-purls)
-	 for purl = (#"replaceFirst" (uri-full new) ".*/obo/" "/obo/")
-	 for id = (#"replaceFirst" purl ".*/" "")
-	 do (format out "<rec><purl>~a</purl><url>http://sw.neurocommons.org/obiterm/~a</url><id>CMUNGALL</id><id>MELANIE</id><type>User_Batch_Add</type></rec>~%" purl id ))
-      (format out "</recs>~%")
-      )
-    (when (set-difference kbprev-purls kb-purls)
-      (format t "Hmm, we seem to have lost some ids (deprecation lossage?): ~%~{~a~%~}"
-	  (set-difference kbprev-purls kb-purls)))    
-    ))
+    (flet ((doit (out)
+
+	     (format out "<recs>~%")
+	     (loop for new in (set-difference kb-purls kbprev-purls)
+		for purl = (#"replaceFirst" (uri-full new) ".*/obo/" "/obo/")
+		for id = (#"replaceFirst" purl ".*/" "")
+		do (format out "<rec><purl>~a</purl><url>http://sw.neurocommons.org/obiterm/~a</url><id>CMUNGALL</id><id>MELANIE</id><id>ALANRUTTENBERG</id><type>User_Batch_Add</type></rec>~%" purl id ))
+	     (format out "</recs>~%")
+	     ))
+      (if (or (eq dest t) (streamp dest))
+	  (doit dest)
+	  (with-open-file (out dest
+			       :direction :output
+			       :if-exists :supersede)
+	    (doit out))
+	  )
+      (when (set-difference kbprev-purls kb-purls)
+	(format t "Hmm, we seem to have lost some ids (deprecation lossage?): ~%~{~a~%~}"
+		(set-difference kbprev-purls kb-purls)))    
+      )))
 
 
 
