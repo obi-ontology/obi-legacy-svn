cd82898c414d9dc0c7c5f1005dba8b5b1399e211
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2008-05-30 03:02:34 +0000

    add comment to file
    git-svn-id: http://svn.code.sf.net/p/obi/code@678 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- trunk/src/tools/build/comment-ids-in-owl-file.lisp
+++ trunk/src/tools/build/comment-ids-in-owl-file.lisp
@@ -1,3 +1,27 @@
+;; Using the labels in kb, reads input file, and puts an xml comment with
+;; the propertly label before properties that use an OBI id, and an xml
+;; comment with the label after the use of any other entity.
+
+;; Comment's won't persis after editing the file and saving in protege,
+;; so this is something to run on release, or when you need an annotated
+;; copy to do some text editing of the OWL files.
+
+;; e.g.
+
+;;   <owl:Class rdf:about="http://purl.obofoundry.org/obo/OBI_0000219"><!-- cellular_feature_identification_objective -->
+;;     <!-- definition --><OBI_0000291 xml:lang="en">Cellular_feature_identification_objective is a biological_feature_identification_objective role describing a study designed to examine or characterize a biological feature monitored at the cellular level, e.g. stage of cell cycle, stage of differentiation.</OBI_0000291>
+;;     <!-- definition source --><OBI_0000279 xml:lang="en">source pending</OBI_0000279>
+;;     <!-- definition editor --><OBI_0000274 xml:lang="en">Jennifer Fostel</OBI_0000274>
+;;     <!-- example of usage --><OBI_0000287 xml:lang="en">example_of_usage to be added</OBI_0000287>
+;;     <!-- preferred term --><OBI_0000288 xml:lang="en">cellular_feature_identification_objective</OBI_0000288>
+;;     <rdfs:subClassOf>
+;;       <owl:Class rdf:about="http://purl.obofoundry.org/obo/OBI_0000015"/><!-- biological_feature_identification_objective -->
+;;     </rdfs:subClassOf>
+;;     <!-- curation status --><OBI_0000281 rdf:resource="http://purl.obofoundry.org/obo/OBI_0000319"/><!-- metadata complete -->
+;;     <rdfs:label xml:lang="en">cellular_feature_identification_objective</rdfs:label>
+;;   </owl:Class>
+
+
 (defun comment-ids-in-owl-file (in-path out-path &optional (kb (load-kb-jena :obi)))
   (let ((labels (rdfs-labels kb)))
     (with-open-file (in in-path)
