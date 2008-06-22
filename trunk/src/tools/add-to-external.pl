780caf67ddd6bf13d9016cf8aaa48491c12c6014
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2008-06-22 03:48:01 +0000

    perl add-to-external.pl child parent [path to external.owl] e.g. perl add-to-external.pl PRO:000000001 CHEBI:23091
    adds, to external.owl:
    
      <owl:Class rdf:about="http://purl.org/obo/owl/PRO#PRO_000000001">
        <rdfs:subClassOf rdf:resource="http://purl.org/obo/owl/CHEBI#CHEBI_23091"/>
        <OBI_0000283 rdf:resource="http://purl.org/obo/owl/PRO"/>
      </owl:Class>
    
    One can use the ids as above, or the URIs
    
    perl add-to-external.pl http://purl.org/obo/owl/PRO#PRO_000000001 CHEBI:23091
    
    would do the same thing.
    
    There is some validation.
    
    - Checks that the child isn't already in external.owl
    - Checks that the parent *is* in external.owl, or is an OBI term.
    - Verifies that the external term is from an ontology that we sanction. Adjust 
     @valid_externals, currently qw(PATO PRO FMA CHEBI GO CL NCBITaxon ENVO) to add more.
    
    It does not validate that the actual id is correct. Future work. Be careful.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/obi/code@761 a3e8fd31-a726-0410-8195-c3f0c8a19530

---
+++ trunk/src/tools/add-to-external.pl
@@ -0,0 +1,92 @@
+use strict;
+
+sub usage ()
+{ print "perl add-to-external.pl child parent [path to external.owl]\ne.g. perl add-to-external.pl PRO:000000001 CHEBI:23091" }
+
+my $child = $ARGV[0];
+my $parent = $ARGV[1];
+my $branchpath = $ARGV[2];
+if (!$branchpath)
+{ $branchpath = `cd  \`dirname $0\`/../ontology/branches/; pwd` ;
+  chomp $branchpath;
+  $branchpath .= "/external.owl";
+}
+my @valid_externals = qw(PATO PRO FMA CHEBI GO CL NCBITaxon ENVO);
+
+my $valid_child_pattern = "(http:\/\/purl.org\/obo\/owl\/"
+    ."(".join("",map "$_#$_\_|", @valid_externals).")(\\d+)\$)|"
+    ."(".join(":(\\d+)\$|", @valid_externals).")";
+
+my $valid_parent_pattern = "(http:\/\/purl.org\/obo\/owl\/"
+    ."(".join("",map "$_#$_\_|", @valid_externals).")(\\d+)\$)|"
+    ."(".join(":(\\d+)\$|", @valid_externals).")|OBI_\\d+\$";
+
+$child =~ /$valid_child_pattern/i or die "child term '$child' doesn't look like a valid id. The term should be from one of the following ontologies: @valid_externals";
+$parent =~ /$valid_parent_pattern/i or die "child term '$child' doesn't look like a valid id. The term should be from one of the following ontologies: @valid_externals";
+-e $branchpath or die "$branchpath doesn't exist";
+
+my ($parenturi,$childuri);
+
+if ($child =~ /(.*?):(.*)$/)
+{ my ($ont,$id) = ($1,$2); 
+  $childuri = "http://purl.org/obo/owl/".uc($ont)."#".uc($ont)."_".$id;
+}
+else { $childuri =~ $child; }
+
+if ($parent =~ /^OBI_(\d+)$/)
+{ $parenturi = "http://purl.obofoundry.org/obo/$1" }
+elsif ($parent =~ /^http/)
+{ $parenturi = $parent }
+elsif( $parent =~ /(.*?):(.*)$/)
+{ my ($ont,$id) = ($1,$2);
+  $parenturi = "http://purl.org/obo/owl/".uc($ont)."#".uc($ont)."_".$id;
+}
+
+(($parent=~ /^OBI_/) || `grep '<owl:Class rdf:about="$parenturi">' $branchpath`) 
+    or die "$parenturi not present. Please use this script to add it then try again.\n";
+
+!`grep '<owl:Class rdf:about="$childuri">' $branchpath` 
+    or die "$childuri already present, so not adding it again";
+
+my $template =<<EOF
+  <owl:Class rdf:about="_CHILD_">
+    <rdfs:subClassOf rdf:resource="_PARENT_"/>
+    <OBI_0000283 rdf:resource="_ONT_"/>
+  </owl:Class>
+EOF
+    ;
+
+my $onturi = $childuri;
+$onturi =~ s/#.*//;
+
+$template =~ s/_ONT_/$onturi/e;
+$template =~ s/_CHILD_/$childuri/e;
+$template =~ s/_PARENT_/$parenturi/e;
+
+print "child: $childuri\n"."parent: $parenturi\n"."ontology: $onturi\n";
+print "adding to $branchpath:\n$template\n";
+
+open EXTERNAL, "<$branchpath" or die "can't open $branchpath to read";
+my @lines = <EXTERNAL>;
+close EXTERNAL;
+open EXTERNAL, ">$branchpath" or die "can't open $branchpath to write";
+foreach (@lines)
+{ if (/<\/rdf:RDF>/)
+  { print EXTERNAL $template,"$_\n" }
+  else 
+  { print EXTERNAL $_}
+}
+close EXTERNAL;
+
+
+__DATA__
+
+
+epithelium = CARO:0000066
+multi-tissue structure = CARO:0000055
+polypeptide CHEBI:15841
+protein PRO:000000001
+cell component = CARO:0000014
+cytoxic T-cell = CL:0000794
+
+
