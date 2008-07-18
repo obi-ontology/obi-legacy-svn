e58d92e5c38a1f437b587e183b5a98bec9f6b924
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2008-07-18 06:51:43 +0000

    check if kb consistent first
    git-svn-id: http://svn.code.sf.net/p/obi/code@857 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- trunk/src/tools/build/add-inferred-superclasses.lisp
+++ trunk/src/tools/build/add-inferred-superclasses.lisp
@@ -1,12 +1,14 @@
 (defun write-inferred-superclasses (&optional (kb (load-kb-jena "obi:newids;obid.owl"))
 				    (path "obi:newids;superclasses.owl"))
-  (with-ontology inferred (:base (format nil "http://purl.obofoundry.org/obo/obi/~a.~a"
-					 (pathname-name (translate-logical-pathname path))
-					 (pathname-type (translate-logical-pathname path)))) 
-      ((loop for class in (descendants !owl:Thing kb)
-	  append
-	  (loop for super in (parents class kb)
-	     collect (class class :partial super))))
-    (check inferred)
-    (write-rdfxml inferred path)))
+  (if (check kb)
+      (with-ontology inferred (:base (format nil "http://purl.obofoundry.org/obo/obi/~a.~a"
+					     (pathname-name (translate-logical-pathname path))
+					     (pathname-type (translate-logical-pathname path)))) 
+	  ((loop for class in (descendants !owl:Thing kb)
+	      append
+	      (loop for super in (parents class kb)
+		 collect (class class :partial super))))
+	(check inferred)
+	(write-rdfxml inferred path))
+      (error "KB isn't consistent, so inferred superclasses won't be correct")))
 				       
