68becfe79fd7cdd4bf9c6d0ec22ad06d86089465
mcourtot <mcourtot@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2009-01-14 00:00:06 +0000

    missing final parenthesis
    git-svn-id: http://svn.code.sf.net/p/obi/code@1350 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- trunk/src/tools/build/generate-obo.lisp
+++ trunk/src/tools/build/generate-obo.lisp
@@ -150,7 +150,7 @@ remark: This file is a subset of OBI adequate for indexing using the OLS service
 		       unless (eq super !ro:relationship)
 		       do (format f "is_a: ~a ! ~a~%" (localname-namespaced super) (or (rdfs-label super) (localname super)))))
 		  (terpri f))
-		)))))
+		))))))
 #|		      
   <owl:Class rdf:about="http://purl.obofoundry.org/obo/OBI_0400082"><!-- photodetector -->
     <IAO_0000117 xml:lang="en">John Quinn</IAO_0000117> <-- definition editor
