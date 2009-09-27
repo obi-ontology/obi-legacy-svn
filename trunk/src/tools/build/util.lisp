dd172b6f2314fc55aba0500d52faf2900839fd2b
alanr <alanr@a3e8fd31-a726-0410-8195-c3f0c8a19530>
2009-09-27 04:04:48 +0000

    obi label source. lets you write:
    !'study design'@obi
    
    and understands this as
    
    !obo:OBI_0500000
    
    See:
    
    (defmethod make-uri-from-label-source ((source (eql :obi)) name actual)
    
    
    git-svn-id: http://svn.code.sf.net/p/obi/code@2088 a3e8fd31-a726-0410-8195-c3f0c8a19530

--- trunk/src/tools/build/util.lisp
+++ trunk/src/tools/build/util.lisp
@@ -22,3 +22,67 @@
   )
 
 
+(defmacro with-obo-metadata-uris (&body body)
+  `(let-uri (("ready-for-release" !obo:IAO_0000122)
+	     ("metadata-complete" !obo:IAO_0000120)
+	     ("metadata-incomplete" !obo:IAO_0000123)
+	     ("pending-final-vetting" !obo:IAO_0000125)
+	     ("uncurated" !obo:IAO_0000124)
+	     ("placeholder" !obo:IAO_0000121)
+	     ("obsolete-class" !<http://www.geneontology.org/formats/oboInOwl#ObsoleteClass>)
+	     ("definition" !obo:IAO_0000115)
+	     ("definition-source" !obo:IAO_0000119)
+	     ("definition-editor" !obo:IAO_0000117)
+	     ("preferred-term" !obo:IAO_0000111)
+	     ("alternative-term" !obo:IAO_0000118)
+	     ("example-of-usage" !obo:IAO_0000112)
+	     ("has-curation-status" !obo:IAO_0000114)
+	     ("editor-note" !obo:IAO_0000116)
+	     ("curation-status" !obo:IAO_0000078)
+	     ("imported-from" !obo:IAO_0000412)
+	     )
+	    ,@(loop for p in (list !obo:IAO_0000078 !obo:IAO_0000112 !obo:IAO_0000118 !obo:IAO_0000111 !obo:IAO_0000117
+				   !obo:IAO_0000119 !obo:IAO_0000115 !obo:IAO_0000412 !obo:IAO_0000114)
+		 collect `(annotation-property ,p))
+	    ,@(loop for i in (list !obo:IAO_0000122 !obo:IAO_0000120 !obo:IAO_0000123 !obo:IAO_0000125 !obo:IAO_0000124 !obo:IAO_0000121) 
+		 collect `(individual ,i (type !curation-status)))
+	    ,@body
+	    ))
+
+(defvar *obi-label-source* nil)
+
+(defmethod make-uri-from-label-source ((source (eql :obi)) name actual)
+  "URI label source for OBI. All labels in OBI or imports are available. Case insensitive matching. Complains if an ambiguous label is used, or if a label is missing, or if actual is supplied and doesn't match the looked up term"
+  (unless *obi-label-source*
+    (let ((table (make-hash-table :test 'equalp)))
+      (let ((kb (load-kb-jena :obim)))
+      (let ((labels (rdfs-labels kb)))
+	(maphash (lambda(uri label) 
+		   (if (gethash label table) 
+		       (unless (eq (gethash label table)  uri)
+			 (print-db (gethash label table) uri label)
+			 (setf (gethash label table) :ambiguous))
+		       (setf (gethash label table) uri)))
+		 labels))
+      (setq *obi-label-source* table))))
+  (let ((found (gethash name *obi-label-source*)))
+    (when (eq found :ambiguous)
+      (if (equalp name "homo sapiens") ; ugly special case until it's fixed.
+	  (return-from make-uri-from-label-source !taxon:9606)
+	  (progn
+	    (warn "Uri label ~a in ~a is ambiguous" name source)
+	    (setq found nil))))
+    (if found
+	(progn
+	  (when actual
+	    (assert (eq found (make-uri nil actual)) (found actual)
+		    "Uri label lookup for '~a' - ~a doesn't match specified actual '~a'"
+		    name found actual))
+	  found)
+	(if actual
+	    (progn
+	      (warn "Uri label lookup for '~a' failed - using provided actual: '~a'" name actual)
+	      (make-uri nil actual))
+	    (error "Couldn't determine which URI was meant by '~a' in ~a" name source)))))
+  
+  
